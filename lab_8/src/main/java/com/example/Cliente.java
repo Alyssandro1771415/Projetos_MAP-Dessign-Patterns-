a7f2e4eee2573d75fd628bd75da2f9a46b1fcddc
danilopvalerio <Danilovalerio2003@gmail.com>
2024-10-27 10:37:49 -0300

    Renomeação de Main.java para Cliente.java

--- lab_8/src/main/java/com/example/Cliente.java
+++ lab_8/src/main/java/com/example/Cliente.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Main {
+public class Cliente {
     public static void main(String[] args) {
         // Usando uma lista normal
         List<String> normalList = new ArrayList<>();
